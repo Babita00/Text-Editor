f58ddf15646f5a100785513c4ef115af91dfcc7d
Babita00 <babitaasth07@gmail.com>
2025-04-22 08:47:01 +0545

    feat: add ToolbarButton component for reusable toolbar actions

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import ToolbarButton from './ToolbarButton';
 import { 
   Bold, Italic, Underline, AlignLeft, AlignCenter, 
   AlignRight, List, ListOrdered, Image, Table, Undo, 
@@ -197,30 +198,6 @@ const Toolbar: React.FC<ToolbarProps> = ({ editorRef }) => {
   );
 };
 
-interface ToolbarButtonProps {
-  icon: React.ReactNode;
-  tooltip: string;
-  active?: boolean;
-  onClick?: () => void;
-}
 
-const ToolbarButton: React.FC<ToolbarButtonProps> = ({ 
-  icon, 
-  tooltip, 
-  active = false, 
-  onClick 
-}) => {
-  return (
-    <button 
-      className={`p-1.5 rounded hover:bg-gray-100 ${active ? 'bg-gray-100' : ''}`} 
-      title={tooltip}
-      onClick={onClick}
-    >
-      <span className={`text-gray-700 ${active ? 'text-blue-600' : ''}`}>
-        {icon}
-      </span>
-    </button>
-  );
-};
 
 export default Toolbar;
